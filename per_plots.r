f1f31d0bca3a6d190db79090f7ca83e5186a48fa
Kevin Gillespie <kgills@gmail.com>
2017-04-20 11:12:18 -0500

    Adjusting colors and range for heat map.

--- per_plots.r
+++ per_plots.r
@@ -30,9 +30,9 @@ for(i in 1:length(file.names)) {
         theme(panel.background=element_blank()) +
         scale_x_continuous(trans = "reverse", breaks=seq(0, -100, -10)) +
         scale_y_continuous(breaks=seq(0, 40, 5)) +
-        stat_contour(bins=10, colour="white", size=0.3) +
+        geom_contour(colour="white", size=0.6, breaks=c(10)) +
         labs(x="Power dBm", y="Channel", subtitle=comments, title=paste(file_sub, "PER heat map", sep=" ")) +
-        scale_fill_gradient2(low="blue", mid="green", high="red", midpoint=50)
+        scale_fill_gradient2(low="dark green", mid="yellow", high="red", midpoint=17, limits=c(0,30), na.value="red")
 
     # Save the heat map
     png(filename=paste(file_sub ,"_heat",".png", sep=""), width=800, height=700)
